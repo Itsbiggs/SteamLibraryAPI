9c8a563e523b7758afff7f198f10f1ae3ba9eec1
Paul Cyrocki <itsbiggs@gmail.com>
2024-06-12 20:05:24 -0400

    add some handling of all games, one game by id, and posting of a game.  Store in memory

--- internal/handlers.go
+++ internal/handlers.go
@@ -1,11 +1,24 @@
 package internal
 
 import (
+	"encoding/json"
 	"fmt"
 	"html"
 	"net/http"
 )
 
+type Game struct {
+	Title    string `json:"title"`
+	ID       string `json:"id"`
+	Played   bool   `json:"played"`
+	Owned    bool   `json:"owned"`
+	Wishlist bool   `json:"wishlist"`
+	Hours    int    `json:"hours"`
+}
+
+var games = []Game{}
+
+// handle request to home page, this eventually will do something besides print a statement, TODO
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
@@ -14,12 +27,44 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "You've made it to %q, also known as Homepage", html.EscapeString(r.URL.Path))
 }
 func GetGamesHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "return all games")
+	//since we are saving to memory we're just going to pull the list, we will need to pull from a DB when implemented, but in this case we're just pulling from the list.
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(games)
 }
+
+// GetGameHandler will let us pull a game based on ID in curl request
 func GetGameHandler(w http.ResponseWriter, r *http.Request) {
-	id := r.PathValue("id")
-	fmt.Fprintf(w, "returning game with ID %s", id)
+	//extract ID from query parameters
+	id := r.URL.Query().Get("id")
+	if id == "" {
+		http.Error(w, "ID is required", http.StatusBadRequest)
+		return
+	}
+	for _, game := range games {
+		if game.ID == id {
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(game)
+			return
+		}
+	}
+	http.Error(w, "Game not found, are you sure you added it?", http.StatusNotFound)
 }
+
+// CreateGamesHandler is a simple post handler that sets the variable game to Game json struct
 func CreateGamesHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Post a new game")
+	var game Game
+	if err := json.NewDecoder(r.Body).Decode(&game); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if game.Owned {
+		game.Wishlist = false
+	}
+	//append the added game to Games
+	games = append(games, game)
+	//report success and encode data
+	w.WriteHeader(http.StatusCreated)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(game)
 }
